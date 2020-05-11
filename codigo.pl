419958c2141d964da7b75287779f447c4f5af139
palbertito <palbertito@hotmail.com>
2020-05-11 15:03:56 +0200

    nombres

---
+++ codigo.pl
@@ -0,0 +1,5 @@
+%Nuestros datos
+alumno_prode('Benavente','Alvarez','Alejandro',160319). %PORTAVOZ
+alumno_prode('Doncel','Aparicio','Alberto',160364).   
+alumno_prode('Lin','Tsai','Alvin',160267).
+%-------------------------------------------
