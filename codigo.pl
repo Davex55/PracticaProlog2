e603052eb667b4cd68a753fede82fd831ddd9935
palbertito <palbertito@hotmail.com>
2020-05-11 20:17:53 +0200

    intentando

--- codigo.pl
+++ codigo.pl
@@ -3,3 +3,24 @@ alumno_prode('Benavente','Alvarez','Alejandro',160319). %PORTAVOZ
 alumno_prode('Doncel','Aparicio','Alberto',160364).   
 alumno_prode('Lin','Tsai','Alvin',160267).
 %-------------------------------------------
+
+eliminar_comodines(Regs, R, L):-
+    Regs =.. A,
+    eliminar_comodin(A, R, L).
+
+eliminar_comodin([],_,_).
+
+eliminar_comodin(A|B, R, L):-
+    comprobar_comodin(A, R, L),
+    eliminar_comodin(B, R, L).
+
+comprobar_comodin(A,R,L):-
+    A =:= '*',
+    B = _,
+    R = R|B.
+
+comprobar_comodin(A,R,L):-
+    A =\= '*',
+    R = R|A,
+    L = L|A.
+
