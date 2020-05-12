4c23fed9266711eaccfdc3bf0c9031705e12ddb0
Alejandro Benavente Alvarez <alejandro.benavente.alvarez@alumnos.upm.es>
2020-05-12 16:04:11 +0200

    implementacion roñosa de eliminar_comodines

--- codigo.pl
+++ codigo.pl
@@ -5,22 +5,59 @@ alumno_prode('Lin','Tsai','Alvin',160267).
 %-------------------------------------------
 
 eliminar_comodines(Regs, R, L):-
-    Regs =.. A,
-    eliminar_comodin(A, R, L).
+    functor(Regs, _, N),
+    %Regs =.. A,
+    %length(R, N),
+	recorrerLista(Regs, R, L, 1, N).
 
-eliminar_comodin([],_,_).
+%recorrerLista(_, _, 0 , _).
+recorrerLista(Regs, [_], _, N ,N) :-
+    arg(N, Regs, X1),
+    comprobar_comodin(X1, B),
+    B = c(),
+    !.
+recorrerLista(Regs, [X1],[X1], N ,N) :-
+    arg(N, Regs, X1),
+    comprobar_comodin(X1, B),
+    B \= c(),
+    !.
+recorrerLista(Regs, [_|R],L, N1, N):-
+	arg(N1, Regs, X1),
+    N2 is N1 + 1,
+    comprobar_comodin(X1, B),
+    B = c(),
+    recorrerLista(Regs, R, L,N2, N).
+recorrerLista(Regs, [X1|R], [X1|L], N1, N):-
+	arg(N1, Regs, X1),
+    N2 is N1 + 1,
+    comprobar_comodin(X1, B),
+    B \= c(), 
+    recorrerLista(Regs, R, L, N2, N).
 
-eliminar_comodin(A|B, R, L):-
-    comprobar_comodin(A, R, L),
-    eliminar_comodin(B, R, L).
+comprobar_comodin(A,B) :-
+      A == *,
+      B = c(),
+      !.
+  
+comprobar_comodin(A,A). 
 
-comprobar_comodin(A,R,L):-
-    A =:= '*',
-    B = _,
-    R = R|B.
-
-comprobar_comodin(A,R,L):-
-    A =\= '*',
-    R = R|A,
-    L = L|A.
+%eliminar_comodines(Regs, R, L):-
+%    Regs =.. A,
+%    eliminar_comodin(A, R, L).
+%
+%eliminar_comodin([],_,_).
+%
+%eliminar_comodin(A|B, R, L):-
+%    comprobar_comodin(A, R, L),
+%    eliminar_comodin(B, R, L).
+%
+%comprobar_comodin(A,R,L):-
+%    A =:= '*',
+%    B = _,
+%    R = R|B.
+%
+%comprobar_comodin(A,R,L):-
+%    A =\= '*',
+%    R = R|A,
+%    L = L|A.
 
